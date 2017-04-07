431078fe28282821215ddda71736bd367b98fbfb
John van Hulsen <john@e-sites.nl>
2017-04-07 16:37:34 +0200

    Add sizes to js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,12 @@
  * Main entry file for app logic
  */
 
+// Config
+app.config = {
+	width_sm: 480,
+	width_md: 640,
+	width_lg: 960
+};
+
 // Apply SVG polyfill to load external SVG's in unsupported browsers
 require('svgxuse');
-
-window.foo = 789;
