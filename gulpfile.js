7c32fd818d5b648967dbb80a40cdf45e76f6b469
Bram Smulders <bram@bramsmulders.com>
2017-10-23 16:36:43 +0200

    Gulpfle comply with eslint standards

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
-'use strict';
-
 // Expose config, Gulp and some plugins that are used by multiple tasks
 const conf = require('./tasks/base/conf.js');
 const gulp = require('gulp');
 const tasker = require('gulp-tasker');
 
 // Load all tasks
-tasker.loadTasks({path: conf.path.tasks});
+tasker.loadTasks({ path: conf.path.tasks });
 
 // Default task when run with 'gulp deploy'
 gulp.task('deploy', gulp.parallel(tasker.getTasks('deploy').tasks));
@@ -16,10 +14,10 @@ gulp.task('default', gulp.parallel(tasker.getTasks('default').tasks));
 
 // Watch task when run with 'gulp watch'
 gulp.task('watch', gulp.series('default', () => {
-	tasker
-		.getTasks('watch')
-		.tasks
-		.forEach((task) => {
-			gulp.watch(task.folders, gulp.parallel(task.tasks));
-		});
+  tasker
+    .getTasks('watch')
+    .tasks
+    .forEach((task) => {
+      gulp.watch(task.folders, gulp.parallel(task.tasks));
+    });
 }));
