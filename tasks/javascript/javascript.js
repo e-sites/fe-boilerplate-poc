1c4ca962ccd0e45f326e280d1d583437e2b18e3f
Bram Smulders <bram@bramsmulders.com>
2017-10-24 16:38:38 +0200

    Implemented webpack-encore for javascript

--- tasks/javascript/javascript.js
+++ tasks/javascript/javascript.js
@@ -7,78 +7,32 @@
 
 const fs = require('fs');
 const gulp = require('gulp');
-const del = require('del');
 const tasker = require('gulp-tasker');
-const gulpif = require('gulp-if');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const uglify = require('gulp-uglify');
-const async = require('async');
-const rev = require('gulp-rev');
+const webpack = require('webpack-stream');
 const notify = require('gulp-notify');
-const { handleSuccess } = require('../base/handlers');
+const notifier = require('node-notifier'); // eslint-disable-line
+const webpackConfig = require('../../webpack.config.js');
 
-const { paths, revisionFiles } = JSON.parse(fs.readFileSync('./package.json')).config;
-const jsFiles = ['app.js'];
-const debug = process.env.NODE_ENV !== 'production';
-
-const cleanjs = (done) => {
-  del([`${paths.dist.js}/*`]);
-  done();
-};
+const { paths } = JSON.parse(fs.readFileSync('./package.json')).config;
 
 const js = (allDone) => {
-  async.each(
-    jsFiles,
-    (file, jsDone) => {
-      // set up the browserify instance on a task basis
-      const b = browserify({
-        entries: `${paths.source.js}/${file}`,
-        debug,
-      });
+  const stream = webpack(webpackConfig);
 
-      const compilejs = b
-        .transform('babelify')
-        .bundle()
-        .on(
-          'error',
-          notify.onError((err) => {
-            const message = err.message ? err.message : err;
-            return {
-              title: 'js',
-              message: `Error: ${(err.annotated ? err.annotated : message)}`,
-            };
-          })
-        )
-        .pipe(source(file))
-        .pipe(buffer())
-        .pipe(gulpif(!debug, uglify()));
+  stream.on('error', notify.onError(error => error));
 
-      // Writing revision in new file?
-      if (revisionFiles) {
-        compilejs
-          .pipe(rev())
-          .pipe(gulp.dest(paths.dist.js))
-          .pipe(rev.manifest())
-          .pipe(gulp.dest(paths.dist.js));
-      } else {
-        compilejs.pipe(gulp.dest(paths.dist.js));
-      }
+  stream.on('close', () => {
+    notifier.notify({
+      title: 'js',
+      message: 'Files written to disk',
+    });
+    allDone();
+  });
 
-      // Add success message :)
-      compilejs.pipe(handleSuccess('js', `JS build succeeded for ${file}`, jsDone()));
-
-      return compilejs;
-    },
-    (err) => {
-      if (err) console.error(err);
-      allDone();
-    }
-  );
+  return stream
+    .pipe(gulp.dest(paths.dist.js));
 };
 
-const jsTask = gulp.series(cleanjs, js);
+const jsTask = gulp.series(js);
 
 gulp.task('js', jsTask);
 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const Encore = require('@symfony/webpack-encore');
+
+const { paths, js: { entries, vendor } } = JSON.parse(fs.readFileSync('./package.json')).config;
+const env = process.env.NODE_ENV === 'production' ? 'production' : 'dev';
+
+Encore.configureRuntimeEnvironment(env);
+
+Encore
+  // directory where all compiled assets will be stored
+  .setOutputPath(paths.dist.js)
+
+  // what's the public path to this directory (relative to your project's document root dir)
+  .setPublicPath('/')
+
+  // empty the outputPath dir before each build
+  .cleanupOutputBeforeBuild()
+
+  .enableSourceMaps(!Encore.isProduction())
+
+  // Split vendor assets from the entries
+  .createSharedEntry('vendor', vendor)
+
+  // create hashed filenames (e.g. app.abc123.css)
+  .enableVersioning();
+
+
+// Dynamically load entry points
+entries.forEach((entry) => {
+  Encore.addEntry(entry.replace('.js', ''), `${paths.source.js}/${entry}`);
+});
+
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
