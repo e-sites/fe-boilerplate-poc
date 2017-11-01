51579f2859c91a4566e66c9f37a0534440283bca
Bram Smulders <bram@bramsmulders.com>
2017-11-01 14:36:02 +0100

    Cleanup and imported important modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,14 @@
-/**
- * Main entry file for app logic
+/*!
+ * @section: Application JS module entry file
+ * @project: PROJECT-NAME
+ * @author: E-sites <frontend@e-sites.nl>
  */
+
+// Add ES5/6/7 polyfills
+import 'babel-polyfill';
+
+// Apply SVG polyfill to load external SVG's in unsupported browsers
 import 'svgxuse';
-import './global/_global';
 
-window.app = {};
-
-// Config
-window.app.config = {
-  breakpoints: {
-    sm: 30,
-    md: 40,
-    lg: 60,
-    xl: 74,
-  },
-};
+// Google Analytics event tracking based on HTML data attributes
+// import 'vestigo';
