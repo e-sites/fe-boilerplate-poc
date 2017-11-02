0f0e298fc8dc931efe85bec82a49d240c93f6e0d
Bram Smulders <bram@bramsmulders.com>
2017-11-02 10:04:26 +0100

    move polyfills to the top

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,10 @@
  * @project: PROJECT-NAME
  * @author: E-sites <frontend@e-sites.nl>
  */
+/* eslint-disable import/first */
+
+// Add ES5/6/7 polyfills
+import './polyfills/core-js';
 
 // Apply SVG polyfill to load external SVG's in unsupported browsers
 import 'svgxuse';
@@ -10,12 +14,10 @@ import 'svgxuse';
 // Google Analytics event tracking based on HTML data attributes
 import vestigo from '@e-sites/vestigo';
 
-// Add ES5/6/7 polyfills
-import './polyfills/core-js';
-
 // Set external links
 import './utilities/setExtLinks';
 
+/* eslint-enable import/first */
 
 /**
  * Initialise imported modules
