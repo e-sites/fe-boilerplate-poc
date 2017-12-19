8f75f2ec56823facb26ade0de5abdf2e2787e545
Iain van der Wiel <iain.vanderwiel@gmail.com>
2017-12-19 14:54:38 +0100

    http to https for error logger

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="theme-color" content="#ffffff">
   <title>Default site</title>
-  <script>window.onerror = new Function("m", "f", "l", "(new Image()).src=\"http://cdn.e-sites.nl/js-error/e.gif?m=\"+encodeURIComponent(m)+\"&f=\"+encodeURIComponent(f)+\"&l=\"+encodeURIComponent(l)+\"&p=\"+encodeURIComponent(document.location.href)+\"&t=1&s=\"+((new Date()).getTime()-" + (new Date()).getTime() + ");window.onerror=null;");</script>
+  <script>window.onerror = new Function("m", "f", "l", "(new Image()).src=\"https://cdn.e-sites.nl/js-error/e.gif?m=\"+encodeURIComponent(m)+\"&f=\"+encodeURIComponent(f)+\"&l=\"+encodeURIComponent(l)+\"&p=\"+encodeURIComponent(document.location.href)+\"&t=1&s=\"+((new Date()).getTime()-" + (new Date()).getTime() + ");window.onerror=null;");</script>
   <script>document.documentElement.className += ' js'; var app = {}, fs = {};</script>
   <link rel="dns-prefetch" href="//ajax.googleapis.com">
   <link rel="dns-prefetch" href="//google-analytics.com">
